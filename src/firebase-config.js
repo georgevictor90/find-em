dbccfb62293d966c7a2cece9ff9ea432d476572e
georgevictor90 <georgevictorl@yahoo.com>
2023-02-09 22:26:03 +0200

    setup firebase firestore

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2vLDiwE58nroTAfes8mkWxY-9Zt9hTew",
+  authDomain: "find-em-c2fbe.firebaseapp.com",
+  projectId: "find-em-c2fbe",
+  storageBucket: "find-em-c2fbe.appspot.com",
+  messagingSenderId: "545079602446",
+  appId: "1:545079602446:web:01ac944d7f2cef3c03bb5f",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
