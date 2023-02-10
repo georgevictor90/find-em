2985dc67fc46a3fa67616753d5b0a2c19dbbde98
georgevictor90 <georgevictorl@yahoo.com>
2023-02-10 15:09:41 +0200

    get storage from firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC2vLDiwE58nroTAfes8mkWxY-9Zt9hTew",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage();
